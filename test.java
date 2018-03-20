3008a09289c11fce5ec7213f6180e0a3213d7247
polarionTest <36860773+polarionTest@users.noreply.github.com>
2018-03-20 14:05:03 -0700

    Update test.java
    another RT

--- test.java
+++ test.java
@@ -3,6 +3,9 @@
 */
 class Main {
   public static void main(String[] args) {
+      /**
+      * @wi.implements desk/KSED-122415:outputs hello world
+      */
       System.out.println("Hello world!");
       /**
       * @wi.implements desk/KSED-122495:outputs hello world
