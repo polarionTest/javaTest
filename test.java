3fb09c0807dfc6c15a493e1286d7045641f59abd
polarionTest <polariontestemail@gmail.com>
2018-08-17 12:56:53 -0700

    testing

--- test.java
+++ test.java
@@ -1,9 +1,8 @@
 /**
-* @wi.implements desk/KSED-122491:asdf asdf asdf
+* @wi desk/KSED-122491:testing
 */
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
-
 }
