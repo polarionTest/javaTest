79fccb51f27eb3ee1ee724bb54edd6428fec5c01
polarionTest <polariontestemail@gmail.com>
2018-08-17 13:06:56 -0700

    test

--- test.java
+++ test.java
@@ -1,8 +1,10 @@
-/**
-* @wi desk/KSED-122491: test again
-*/
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
 }
+
+/**
+* @wi desk/KSED-122491: test again
+*/
