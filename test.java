19a4a347d9ab4078cb67da334193552f12215ed2
polarionTest <polariontestemail@gmail.com>
2019-03-22 12:43:19 -0700

    test comments

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
 
-
+// Main class
 class Main {
 
-
+  // main method
   public static void main(String[] args) {
 
-
+      // logging "Hello world!"
       System.out.println("Hello world!");
 
 
