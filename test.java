b2d55937a32e0e180d41147296ae94d5118b9164
PJC-1 <polariontestemail@gmail.com>
2019-03-22 12:31:56 -0700

    cleaned up test.java file

--- test.java
+++ test.java
@@ -1,23 +1,10 @@
-/**
-* @wi.implements desk/KSED-122491:outputs hello world
-*/
+
 class Main {
 
-  /**
-  * @wi.implements desk/KSED-122655:outputs something diff
-  */
   public static void main(String[] args) {
-      /**
-      * @wi.implements desk/KSED-122415:outputs hello world
-      */
-      System.out.println("Hello world!");
-      /**
-      * @wi.implements desk/KSED-122495:outputs hello world
-      */
 
-      /**
-      * @wi.implements desk/KSED-122653:outputs something diff
-      */
+      System.out.println("Hello world!");
+
   }
 
 
